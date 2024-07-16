6855e55e4c00c6f42962c6307feab7767a1b0376
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-07-16 17:37:54 -0300

    Add files via upload

---
+++ Exercicio02/script.js
@@ -0,0 +1,8 @@
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Hora do Desafio";
+
+function clicarNoConsole(){
+    console.log("O botão foi clicado");
+}
