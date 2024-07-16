3adb9f49c4dccaa75e99a9d827d9476f7a6a0f1a
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-07-16 17:22:43 -0300

    Delete Exercicio02 directory

--- Exercicio02/script.js
+++
@@ -1,6 +0,0 @@
-// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
-
-function clicarNoConsole(){
-    console.log("O botão foi clicado");
-}
-    
